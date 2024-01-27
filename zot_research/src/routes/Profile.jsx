9899ee4563277fd0a0f654d0e0cfcddbfbb14e14
Pranav Thurgam <109917213+PranavThurgam05@users.noreply.github.com>
2024-01-27 11:25:34 -0800

    Add profile pic/bar + description box to profile page

--- zot_research/src/routes/Profile.jsx
+++ zot_research/src/routes/Profile.jsx
@@ -1,40 +1,59 @@
-import React from 'react'
-import profpic from '../assets/profile_pic.jpeg'
-import './Profile.css'
+// Profile.js
+
+import React from 'react';
+import profpic from '../assets/profile_pic.jpeg';
+import './Profile.css';
 
 function Profile() {
-   return (
-     <>
-       <div>
-       <h1 class = "profheading">My Profile</h1>
-       <div class = "imagecrop">
-           <img src={profpic} className="profile_pic.jpeg" alt="Profile Picture" />
-           </div>
-         <a>
-           <p className="proftext">
-               Enter Text Here
-               <br/>
-               Enter Description Here
-               <br/>
-               Enter Class/Year Here
-               <br/>
-               <button class = "editbutton">Edit</button>
-           </p>
-         </a>
-       </div>
+  return (
+    <div id="root">
+      <div className='box' id="row1">
 
-       <div  class = "research"> 
-        <p>Research Interests: (Enter research interests here) </p>
-       </div>
-
-       <div className="embed">
-      <p>Resume:</p>
-      <iframe src="https://docs.google.com/document/d/1JymkivBFogSjGwIEufmdmFrW2YOxofThw4iV2hac8Fo/edit?usp=sharing" height="400" width="400"></iframe>
       </div>
-      
-    
-     </>
-   )
+      <div className="box" id="box1">
+      <div className="box" id="box2">
+        <div class="inner-box">
+          <div className="text">
+            <p className="Name">
+              Name: _____
+            </p>
+            <p className="Other_Details">
+              <br />
+              University: _____
+              <br />
+              Major: _____
+              <br />
+              Degree: _____
+              <br />
+            </p>
+              <button className="editbutton">Edit</button>
+          </div>
+          <div className="imagecrop">
+            <img src={profpic} className="profile_pic.jpeg" alt="Profile Picture" />
+          </div>
+        </div>
+        <div class="also-inner-box">
+          <p className="Name">
+            Description
+          </p>
+        </div>
+      </div>
+      </div>
+      <div className="box" id="box3">
+      <div className='box' id="row2">
+
+      </div>
+        <div className="embed">
+          <iframe src="https://docs.google.com/document/d/1JymkivBFogSjGwIEufmdmFrW2YOxofThw4iV2hac8Fo/edit?usp=sharing" height="700" width="600"></iframe>
+        </div>
+      </div>
+
+      <div className="research">
+        <p>Research Interests: (Enter research interests here) </p>
+      </div>
+
+    </div>
+  );
 }
 
-export default Profile
+export default Profile;
