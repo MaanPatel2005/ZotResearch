ec1714aaa718fc066a7379ecf8e9c84d83aaa04e
Mitali <mitaliactive@gmail.com>
2024-01-27 21:30:43 -0800

    Integrated login page

--- zot_research/src/App.jsx
+++ zot_research/src/App.jsx
@@ -2,8 +2,15 @@ import Navbar from './assets/Navbar';
 import './App.css'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Profile from './routes/Profile';
+<<<<<<< Updated upstream
 import React from 'react';
 import AddPost from './routes/Add_Post'
+=======
+import React, { useContext } from 'react';
+import AddPost from './routes/Add_Post';
+import { Auth } from './auth';
+import Login from './login';
+>>>>>>> Stashed changes
 
 const App = () => {
   const userIsAdmin = true;
@@ -17,6 +24,14 @@ const App = () => {
         <Route path="/Profile" element={<Profile />} />
         <Route path="/Messaging" element={<div></div>} />
         <Route path="/Post" element={<AddPost />} />
+<<<<<<< Updated upstream
+=======
+        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<Auth />} />
+        {/* Add these routes if you have corresponding components */}
+        {/* <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} /> */}
+>>>>>>> Stashed changes
       </Routes>
     </Router>
   );
--- zot_research/src/main.jsx
+++ zot_research/src/main.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import App from './App.jsx'
+import App from './App.jsx';
+import Login from './login';
 import './index.css';
 import ReactDOM from 'react-dom';
 import LoginPage from './login.jsx';
@@ -7,6 +8,11 @@ import LoginPage from './login.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+<<<<<<< Updated upstream
     <LoginPage />
+=======
+    <Login />
+    <App />
+>>>>>>> Stashed changes
   </React.StrictMode>,
 );
