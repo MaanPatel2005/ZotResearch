aaf2aa191a6fd7759359100572be41c7cd7d3c6e
Mitali <mitaliactive@gmail.com>
2024-01-27 20:06:15 -0800

    Add Post

--- zot_research/src/main.jsx
+++ zot_research/src/main.jsx
@@ -2,11 +2,12 @@ import React from 'react';
 import App from './App.jsx'
 import './index.css';
 import ReactDOM from 'react-dom';
+import TextChat from './TextChat.jsx'
 // import LoginPage from './login.jsx';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <TextChat />
   </React.StrictMode>,
 );
