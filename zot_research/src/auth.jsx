78c60608aaafb535ee536635abc36e21e6ae3a70
Pranav Thurgam <109917213+PranavThurgam05@users.noreply.github.com>
2024-01-27 20:33:56 -0800

    Starting firestore.

--- zot_research/src/auth.jsx
+++ zot_research/src/auth.jsx
@@ -1,18 +1,47 @@
 import { auth , googleProvider} from "./firebase";
 import { createUserWithEmailAndPassword,signInWithPopup, signOut } from "firebase/auth";
-import { useState } from "react";
+import React, { useState, useEffect } from "react";
+import 'firebase/firestore';
 
 export const Auth = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-    console.log(auth?.currentUser?.email);
-  const signIn = async () => {
-    try {
-    await createUserWithEmailAndPassword(auth, email, password);
-    } catch (err){
-      console.error(err);
-    }
-  };
+  const [user, setUser] = useState(null);
+  // const [email, setEmail] = useState("");
+  // const [password, setPassword] = useState("");
+  //   console.log(auth?.currentUser?.email);
+  // const signIn = async () => {
+  //   // try {
+  //   // await createUserWithEmailAndPassword(auth, email, password);
+  //   // } catch (err){
+  //   //   console.error(err);
+  //   // }
+  // };
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged(async(userAuth) => {
+      if (userAuth) {
+        const userRef = firestore.collection('users').doc(userAuth.uid);
+        const snapshot = await userRef.get();
+
+        if (!snapshot.exists) {
+          const {displayName, email, photoURL} = userAuth;
+          try {
+            await userRef.set({
+              displayName,
+              email,
+              photoURL
+            });
+          } catch(error) {
+            console.error('Error creating user document', error.message);
+          }
+        }
+
+        setUser(userAuth);
+      } else {
+        setUser(null);
+      }
+    });
+
+    return () => unsubscribe();
+  }, []);
   const signInWithGoogle = async () => {
     try {
     await signInWithPopup(auth,googleProvider);
@@ -29,13 +58,13 @@ export const Auth = () => {
   };
   return (
     <div>
-      <input placeholder="Email.." onChange={(e) => setEmail(e.target.value)} />
+      {/* <input placeholder="Email.." onChange={(e) => setEmail(e.target.value)} />
       <input
         type="password"
         placeholder="Password.."
         onChange={(e) => setPassword(e.target.value)}
       />
-      <button onClick={signIn}> Signin</button>
+      <button onClick={signIn}> Signin</button> */}
       <button onClick={signInWithGoogle}> Signin with google</button>
       <button onClick={logOut}> logOut</button>
     </div>
